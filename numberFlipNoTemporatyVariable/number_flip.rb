d9c921bac67aa75dac248b430e0d45c616c3f498
Anders Palm <ang.palm@hotmail.com>
2018-08-20 00:10:48 -0400

    added ruby version of number swap without a temporary variable

---
+++ numberFlipNoTemporatyVariable/number_flip.rb
@@ -0,0 +1,20 @@
+// For reusability, I have used a proc as this is part of a larger sbset of algorithyms I have been working on. 
+// A Proc offers a useful way of reusing methods with different block implementations.
+
+number_flip_proc = Proc.new { |item|
+  fi = item[0]
+  si = item[1]
+  fi = fi.to_s + si.to_s
+  si = fi[0...(si.to_s.length.to_i)]
+  fi = fi[(fi.to_s.length.to_i - 1)..(fi.to_s.length)]
+  item[0] = fi
+  item[1] = si
+}
+
+def literals_manuipulation(input_array, input_proc)
+  input_array.each.with_index do |item|
+    item = input_proc.call(item)
+  end
+end
+
+literals_manuipulation([[1,2],[2,3]], number_flip_proc)
